3a231fbc3269cde997bc1842f2995e28dbec50ac
Bappy <bappybd@gmail.com>
2012-10-09 15:14:58 +0600

    added jQuery

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
@@ -15,6 +16,11 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+   
+   <?php 
+      $cs = Yii::app()->clientScript; 
+      $cs->registerCoreScript('jquery');
+   ?>
 </head>
 
 <body>
